e9bddd6f11f7219c71efac9b2dfa9ed1d651fda0
Mahdi <m.zandakbari@gmail.com>
2022-07-03 17:54:14 +0430

    HttpRequestDataSourceExtension

--- MZSimpleDynamicLinq.HttpRequestExtensions/HttpRequestExtensions.cs
+++ MZSimpleDynamicLinq.HttpRequestExtensions/HttpRequestExtensions.cs
@@ -1,9 +1,78 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using MZSimpleDynamicLinq.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MZSimpleDynamicLinq.HttpRequestExtensions
 {
-    public class HttpRequestExtensions
+    public static class HttpRequestDataSourceExtension
     {
+        public static LinqDataRequest ToDataSourceRequest(this HttpRequest request)
+        {
 
+            var draw = request.Query["draw"].FirstOrDefault();
+            var start = request.Query["start"].FirstOrDefault();
+            var length = request.Query["length"].FirstOrDefault();
+
+            var searchValue = request.Query["search[value]"].FirstOrDefault();
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
+            List<Sort> sorts = new List<Sort>();
+            //For now just support 20 filters
+            for (int i = 0; i < 20; i++)
+            {
+                var sortColumn = request.Query["columns[" + request.Query["order[" + i + "][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var sortColumnDirection = request.Query["order[" + i + "][dir]"].FirstOrDefault();
+                if (string.IsNullOrEmpty(sortColumnDirection))
+                {
+                    break;
+                }
+                Sort sort = new Sort()
+                {
+                    Dir = sortColumnDirection,
+                    Field = sortColumn
+                };
+                sorts.Add(sort);
+            }
+
+            Filter filter = new Filter();
+
+            var fls = new List<Filter>();
+            for (int i = 0; i < 20; i++)
+            {
+                var filterColumnName = request.Query["columns[" + request.Query["order[" + i + "][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var columnSearchable = request.Query["columns[" + i + "][searchable]"].FirstOrDefault();
+                var filterColumnValue = request.Query["columns[" + i + "][search][value]"].FirstOrDefault();
+
+                if (string.IsNullOrEmpty(filterColumnName) || string.IsNullOrEmpty(filterColumnValue) || columnSearchable != "true")
+                {
+                    continue;
+                }
+                fls.Add(new Filter()
+                {
+                    Logic = "and",
+                    Field = filterColumnName,
+                    Operator = "contains",
+                    Value = filterColumnValue.Replace("(", "").Replace(")", "")
+                });
+            }
+            if (fls.Count > 0)
+            {
+                filter.Logic = "and";
+                filter.Filters = fls;
+            }
+
+
+            return new LinqDataRequest()
+            {
+                Skip = skip,
+                Take = pageSize,
+                Filter = filter,
+                Sort = sorts
+            };
+        }
     }
 }
